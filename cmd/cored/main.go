51783e3403435b97c0c112363a8e7dc2fb62360e
Boyma Fahnbulleh <boymanjor@icloud.com>
2017-03-09 07:59:14 -0800

    wip

--- cmd/cored/main.go
+++ cmd/cored/main.go
@@ -225,6 +225,8 @@ func runServer() {
 		}
 
 		server.TLSConfig = &tls.Config{
+			ClientAuth:   tls.RequireAndVerifyClientCert,
+			ClientCAs:    loadRootCAs(rootCA),
 			Certificates: []tls.Certificate{cert},
 		}
 		err = server.ListenAndServeTLS("", "") // uses TLS certs from above
